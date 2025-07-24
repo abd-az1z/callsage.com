2381d62e162966cbe941a35568da36473be5ab40
Abdulaziz <mohdabdulaziz2023@gmail.com>
2025-07-24 14:44:02 -0700

    12-agents-data-table

---
+++ src/modules/agents/ui/components/columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+// Use inline type to match DataTable data
+type AgentRow = {
+  name: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  instructions: string;
+  meetingCount: number;
+};
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { CornerDownRight, VideoIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+export const columns: ColumnDef<AgentRow>[] = [
+  {
+    accessorKey: "name",
+    header: "Agent Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2">
+          <GeneratedAvatar
+            variant="bottsNeutral"
+            seed={row.original.name}
+            className="size-6"
+          />
+          <span className="font-semibold capitalize">{row.original.name}</span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <CornerDownRight className="size-3 text-muted-foreground " />
+          <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize  ">
+            {row.original.instructions}
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: ({ row }) => (
+      <Badge variant="outline" className="flex items-center gap-x-2 [&>svg]:size-4 ">
+        <VideoIcon className="text-blue-700  " />
+        5 meetings
+        {/* {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "meeting" : "meetings"} */}
+      </Badge>
+    ),
+  },
+];
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,16 +2,24 @@
 
 import { ErrorState } from "@/components/errorState";
 import { LoadingState } from "@/components/loadingState";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { EmptyState } from "@/components/emptyState";
+
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      {JSON.stringify(data, null, 2)}
+    <div className="flex-1 p-4 md:px-8 flex flex-col gap-y-4 ">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 &&(
+        <EmptyState title="Create your first agent"
+        description="Create an agent to join meetings. Each agent will follow your instructions and can interact with participants during the call. " />
+      )}
     </div>
   );
 };
