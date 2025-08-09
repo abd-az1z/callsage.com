015fc17be81882cf3869c36f95530dc6e6c33121
Abdulaziz <mohdabdulaziz2023@gmail.com>
2025-08-09 11:52:36 -0700

    30-readme.md

--- src/app/call/[meetingId]/page.tsx
+++ src/app/call/[meetingId]/page.tsx
@@ -5,19 +5,13 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+type CallRouteParams = { params: { meetingId: string } };
 
-interface Props {
-  params: {
-    meetingId: string;
-  };
-}
-
-
-const Page = async ({ params }: Props) => {
+export default async function Page({ params }: CallRouteParams) {
   const { meetingId } = params;
 
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: headers(),
   });
 
   if (!session) {
@@ -26,16 +20,56 @@ const Page = async ({ params }: Props) => {
 
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
-    trpc.meetings.getOne.queryOptions({
-      id: meetingId
-    })
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
   );
 
   return (
-    <HydrationBoundary  state={dehydrate(queryClient)}>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CallView meetingId={meetingId} />
     </HydrationBoundary>
   );
-};
+}
 
-export default Page;
+
+
+// import { auth } from "@/lib/auth";
+// import { CallView } from "@/modules/call/ui/views/CallView";
+// import { getQueryClient, trpc } from "@/trpc/server";
+// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import { headers } from "next/headers";
+// import { redirect } from "next/navigation";
+
+
+// interface Props {
+//   params: {
+//     meetingId: string;
+//   };
+// }
+
+
+// const Page = async ({ params }: Props) => {
+//   const { meetingId } = params;
+
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
+
+//   if (!session) {
+//     redirect("/sign-in");
+//   }
+
+//   const queryClient = getQueryClient();
+//   void queryClient.prefetchQuery(
+//     trpc.meetings.getOne.queryOptions({
+//       id: meetingId
+//     })
+//   );
+
+//   return (
+//     <HydrationBoundary  state={dehydrate(queryClient)}>
+//       <CallView meetingId={meetingId} />
+//     </HydrationBoundary>
+//   );
+// };
+
+// export default Page;
